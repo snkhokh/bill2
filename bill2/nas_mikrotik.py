836502c1a380c6111847cd1e38fcf7fc831a0b4f
SN-OIS <snkhokh@gmail.com>
2014-06-09 23:39:38 +0400

    home commit

--- bill2/nas_mikrotik.py
+++ bill2/nas_mikrotik.py
@@ -10,8 +10,11 @@ class MikroNas(Nas):
         self.__hw_nas.login(login, passwd)
 
     def _hw_get_all_hosts(self):
-        lists = {item['list']:item for item in self.__hw_nas.response_handler(self.__hw_nas.talk(('/ip/firewall/address-list/print','?dynamic=false','?disabled=false')))}
-        print lists.keys()
+        lists = {}
+        for item in self.__hw_nas.response_handler(self.__hw_nas.talk(('/ip/firewall/address-list/print','?dynamic=false','?disabled=false'))):
+            if item['list'] in lists: lists[item['list']][item['address']] = item['.id']
+            else: lists[item['list']] = {item['address']:item['.id']}
+        print lists
 
     def periodic_proc(self):
         self._hw_get_all_hosts()
